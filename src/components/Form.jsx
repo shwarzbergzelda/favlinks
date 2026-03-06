66748d56e4aa5c935cef76517c7fc1ad68a7fbe9
zelda <shwarzbergzelda@gmail.com>
2026-03-05 20:02:23 -0500

    create initial form structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,18 @@
 function Form() {
     return (
-        <form></form>
+        <form>
+            <label for="linkName">Link name:</label>
+            <input type="text" id="linkName" name="linkName" />
+            <br />
+            <br />
+
+            <label for="linkURL">Link URL:</label>
+            <input type="text" id="linkURL" name="linkURL" />
+            <br />
+            <br />
+
+            <input type="submit" value="Submit" />
+        </form>
     )
 }
 
