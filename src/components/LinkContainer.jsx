6049561acf828aea98da8937f704167104cb5e77
zelda <shwarzbergzelda@gmail.com>
2026-03-05 19:51:00 -0500

    render initial LinkContainer with Form and Table components

--- src/components/LinkContainer.jsx
+++ src/components/LinkContainer.jsx
@@ -1,7 +1,14 @@
+import Table from "./Table"
+import Form from "./Form"
+
 function LinkContainer() {
     return (
         <div>
-            Link Container
+            <h1>My Favorite Links</h1>
+            <p>Add a new link with a name and URL to the table!</p>
+            <Table />
+            <h1>Add New</h1>
+            <Form />
         </div>
     )
 }
