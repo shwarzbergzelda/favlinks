de9148325562e4ce1d65b5c00e459f16b6d8ed38
zelda <shwarzbergzelda@gmail.com>
2026-03-05 20:23:40 -0500

    change for to htmlFor for jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,12 @@
 function Form() {
     return (
         <form>
-            <label for="linkName">Link name:</label>
+            <label htmlFor="linkName">Link name:</label>
             <input type="text" id="linkName" name="linkName" />
             <br />
             <br />
 
-            <label for="linkURL">Link URL:</label>
+            <label htmlFor="linkURL">Link URL:</label>
             <input type="text" id="linkURL" name="linkURL" />
             <br />
             <br />
