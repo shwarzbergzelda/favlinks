9bf4fbadfa815403d8c2b0038e0dfa3e77627571
zelda <shwarzbergzelda@gmail.com>
2026-03-05 19:56:33 -0500

    create and render TableHeader and TableBody components in Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,31 @@
+function TableHeader() {
+    /* responsible for rendering the head of our table with the appropriate columns */
+    
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>URL</th>
+                <th>Remove</th>
+            </tr>
+        </thead>
+    )
+}
+
+function TableBody() {
+    /* responsible for rendering the data for our table */
+
+    return (
+        <tbody></tbody>
+    )
+}
+
 function Table() {
     return (
-        <table></table>
+        <table>
+            <TableHeader />
+            <TableBody />
+        </table>
     )
 }
 
